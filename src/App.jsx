244b1e403797fafae822a0a20b634b198a3a2b79
Saad Khaleeq <saadkhaleeq610@gmail.com>
2024-06-15 03:22:05 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  console.log(import.meta.VITE_APPWRITE_URL);
+
+  return (
+    <>
+    <h1>A blog using appwrite</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/appwrite/config.js
@@ -0,0 +1,28 @@
+import conf from '../conf/conf.js'
+import { Client, ID, Databases, Storage, Query } from 'appwrite'
+
+export class Service {
+    client = new Client();
+    databases;
+    bucket;
+    constructor(){
+        this.client
+        .setEndpoint(conf.endpoint)
+        .setProject(conf.projectId);
+
+        this.databases = new Databases(this.client);
+        this.bucket = new this.Storage(this.client);
+    }
+    
+    async createPost({title, slug, content, featuredImage, status, userId})
+
+    try {
+        
+    } catch (error) {
+        console.log("Appwrite service :: createPost :: error", error);
+    }
+
+}
+
+const service = new Service;
+export default Service;
