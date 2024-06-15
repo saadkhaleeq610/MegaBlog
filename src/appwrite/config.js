3dac355ff71d771c5d8bb87a5d63ae0c2101f5f3
Saad Khaleeq <saadkhaleeq610@gmail.com>
2024-06-15 03:41:43 -0700

    Update config.js

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -14,15 +14,35 @@ export class Service {
         this.bucket = new this.Storage(this.client);
     }
     
-    async createPost({title, slug, content, featuredImage, status, userId})
+async createPost({title, slug, content, featuredImage, status, userId}){
 
+        try {
+            return await this.databases.createDocument(conf.appwriteDatabaseId, conf.appwriteCollectionId, slug, {
+                title,
+                content,
+                featuredImage,
+                status,
+                userId,
+
+            })
+        } catch (error) {
+            console.log("Appwrite service :: createPost :: error", error);
+        }
+
+async updatePost(slug, {title, content, featuredImage, status, userId}){
     try {
-        
-    } catch (error) {
-        console.log("Appwrite service :: createPost :: error", error);
-    }
+        return await this.databases.updateDocument(
+            conf.appwriteDatabaseId,
+            conf.appwriteCollectionId,
+            slug,
 
+        )
+    } catch (error) {
+        
+    }
 }
+
+    }
 
 const service = new Service;
 export default Service;
