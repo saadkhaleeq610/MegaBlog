494447a5d2f1b7b83046522b3b954f38a993ee78
Saad Khaleeq <saadkhaleeq610@gmail.com>
2024-06-15 04:14:52 -0700

    Update config.js

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -47,6 +47,43 @@ async updatePost(slug, {title, content, featuredImage, status, userId}){
     }
 }
 
+async deletePost(slug){
+    try {
+        await this.databases.deleteDocument(
+            conf.appwriteDatabaseId,
+            conf.appwriteCollectionId,
+            slug
+        )
+        return true;
+    } catch (error) {
+        console.log("Appwrite service :: deletePost :: error", error); 
+        return false;
+    }
+}
+
+async getPost(slug){
+    try {
+        return await this.databases.getDocument(
+            conf.appwriteDatabaseId,
+            conf.appwriteCollectionId,
+            slug
+        )
+    } catch (error) {
+        console.log("Appwrite service :: getPost :: error", error); 
+    }
+}
+
+async getPosts(queries = [Query.equal("status", "active")]){
+    try {
+        return await this.databases.listDocuments(
+            conf.appwriteDatabaseId,
+            conf.appwriteCollectionId,
+            queries,
+        )
+    } catch (error) {
+        console.log("Appwrite service :: getPosts :: error", error);
+    }
+}
 }
 
 const service = new Service;
