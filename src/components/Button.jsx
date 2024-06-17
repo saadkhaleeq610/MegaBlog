4a6a391637cf508ef67b7020b650f60da84080f1
Saad Khaleeq <saadkhaleeq610@gmail.com>
2024-06-17 07:11:57 -0700

    btn comp updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,17 @@
 import React, { Children } from 'react'
 
-function Button() {
+function Button({
     Children,
-
+    type = 'button',
+    bgColor = 'bg-blue-600',
+    textColor = 'text-white',
+    className = '',
+    ...props
+}) {
   return (
-    <div>
-      Button
-    </div>
+    <button className={`px-4 py-2 rounded-lg ${className} ${bgColor} ${textColor} {...props}`}>
+      {Children}
+    </button>
   )
 }
 
