a2babd2e7b9e6fee2e4b3775e21911f65d50719e
Saad Khaleeq <saadkhaleeq610@gmail.com>
2024-06-18 09:07:49 -0700

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -33,6 +33,19 @@ function Login() {
                         <logo width="100%"/>
                     </span>
                 </div>
+                <h2 className="text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
+                <p className="mt-2 text-center text-base text-black/60">
+                Don&apos;t have any account?&nbsp;
+                <Link to="/signup" className="font-medium text-primary transition-all duration-200 hover:underline">
+                Sign Up
+                </Link>
+                </p>
+                {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+                <form onSubmit={handleSubmit(login)} className="mt-8">
+                    <div className="space-y-5">
+                        <Input label="Email: " placeholder="Enter your email:" type="email" {...register("email")}></Input>
+                    </div>
+                </form>
             </div>
         </div>
     )
