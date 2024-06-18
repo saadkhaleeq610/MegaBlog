46624048c182b4d17f53d349e276496a2bdcaf13
Saad Khaleeq <saadkhaleeq610@gmail.com>
2024-06-18 07:14:40 -0700

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -11,7 +11,29 @@ function Login() {
     const dispatch = useDispatch();
     const {register, handleSubmit} = useForm();
     const [error, setError] = useState("");
+
+    const login = async(data) => {
+        setError("")
+        try {
+            const session = await authService.login(data)
+            if (session){
+                const userData = await authService.getCurrentUser()
+                if(userData) dispatch(authLogin(userData));
+                navigate("/")
+            }
+        } catch (error) {
+            setError(error.message)
+        }
+    }
     return(
-        <div>Login</div>
+        <div className="flex items-center justify-center w-full">
+            <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+                <div className="mb-2 flex justify-center">
+                    <span className="inline-block w-full max-w-[100px]">
+                        <logo width="100%"/>
+                    </span>
+                </div>
+            </div>
+        </div>
     )
 }
