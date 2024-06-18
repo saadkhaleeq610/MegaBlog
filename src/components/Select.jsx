06c1b693eb5fe7cb9301e32ea865965d0ee41d46
Saad Khaleeq <saadkhaleeq610@gmail.com>
2024-06-17 23:24:22 -0700

    COMIT

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -13,11 +13,13 @@ function Select({
       {label && <label htmlFor={id} className=''></label>}
       <select {...props} ref={ref} className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-ful ${className}`}>
         {options?.map((option) => (
-            
+            <option key={option}>
+                {option}
+            </option>
         ))}
       </select>
     </div>
   )
 }
 
-export default Select
+export default (Select)
