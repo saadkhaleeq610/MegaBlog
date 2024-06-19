9b5a67f1bc78bea97e14109a9eb60d6835dd9169
Saad Khaleeq <saadkhaleeq610@gmail.com>
2024-06-19 00:11:46 -0700

    Update RTE.jsx

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -3,7 +3,17 @@ import {Editor} from '@tinymce/tinymce-react';
 
 export default function RTE() {
   return (
-    <Editor>
+    <Editor 
+    initialValue='default-value' 
+    init={
+        {branding: false,
+        height: 500,
+        menubar: true,
+        plugins: [
+            
+        ]
+        }
+    }>
       
     </Editor>
   )}
