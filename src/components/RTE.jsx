36b94deab188293648ee2d258776a5e005a5dff5
Saad Khaleeq <saadkhaleeq610@gmail.com>
2024-06-19 00:45:35 -0700

    Update RTE.jsx

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -4,39 +4,5 @@ import {Controller} from 'react-hook-form';
 
 export default function RTE({name, control, label, defaultValue = ""}) {
   return (
-    <Editor 
-    initialValue='default-value' 
-    init={
-        {branding: false,
-        height: 500,
-        menubar: true,
-        plugins: [
-            
-            "image",
-            "advlist",
-            "autolink",
-            "lists",
-            "link",
-            "image",
-            "charmap",
-            "preview",
-            "anchor",
-            "searchreplace",
-            "visualblocks",
-            "code",
-            "fullscreen",
-            "insertdatetime",
-            "media",
-            "table",
-            "code",
-            "help",
-            "wordcount",
-            "anchor",
-        ], 
-        toolbar: "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
-        content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
-        }
-    }>
-      
-    </Editor>
+    
   )}
