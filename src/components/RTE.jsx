fbd6bed868295f4465135d7b81097e56f4640f54
Saad Khaleeq <saadkhaleeq610@gmail.com>
2024-06-19 00:49:32 -0700

    Update RTE.jsx

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -4,5 +4,7 @@ import {Controller} from 'react-hook-form';
 
 export default function RTE({name, control, label, defaultValue = ""}) {
   return (
-    
+    <div className='w-full'>
+      {label && <label className='text-sm text-gray-600'>{label}</label>}
+    </div>
   )}
