175669648ea6955bb774bdec69db60592cd39d2f
Saad Khaleeq <saadkhaleeq610@gmail.com>
2024-06-19 02:24:33 -0700

    Update PostForm.jsx

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -6,6 +6,11 @@ import { useNavigate} from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 function PostForm() {
+  const {register, handleSubmit, watch, setValue, control, getValues} = useForm({
+    defaultValues: {
+      
+    }
+  })
   return (
     <div>
       
