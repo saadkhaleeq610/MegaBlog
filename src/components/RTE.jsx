3a3b17029d85c1b58de7961f81e7bd8c110227f6
Saad Khaleeq <saadkhaleeq610@gmail.com>
2024-06-19 00:14:46 -0700

    Update RTE.jsx

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -11,7 +11,29 @@ export default function RTE() {
         menubar: true,
         plugins: [
             
-        ]
+            "image",
+            "advlist",
+            "autolink",
+            "lists",
+            "link",
+            "image",
+            "charmap",
+            "preview",
+            "anchor",
+            "searchreplace",
+            "visualblocks",
+            "code",
+            "fullscreen",
+            "insertdatetime",
+            "media",
+            "table",
+            "code",
+            "help",
+            "wordcount",
+            "anchor",
+        ], 
+        toolbar: "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+        content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
         }
     }>
       
