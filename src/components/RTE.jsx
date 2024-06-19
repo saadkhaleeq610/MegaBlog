2624c4e78b1b63eefaae5d753cdc150f3bd5a894
Saad Khaleeq <saadkhaleeq610@gmail.com>
2024-06-19 00:57:28 -0700

    Update RTE.jsx

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -8,7 +8,18 @@ export default function RTE({name, control, label, defaultValue = ""}) {
       {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
 
       <Controller
-      
+      name={name || "content"}
+      control={control}
+      render={({field: {onChange}}) => (
+        <Editor initialValue={defaultValue}
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: []
+        }}>
+
+        </Editor>
+      )}
       />
     </div>
   )}
