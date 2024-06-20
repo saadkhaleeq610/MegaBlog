3e1df3440dd25a764a6ff05a2570aa6564d153e9
Saad Khaleeq <saadkhaleeq610@gmail.com>
2024-06-20 07:07:58 -0700

    created new comps

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,11 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
-import Logo from "Logo"
+import Logo from "./Logo"
+import PostForm from "./post-form/PostForm"
+import Login from "../pages/Login"
+import Signup from "../pages/Signup"
+import Post from './Post'
+import PostCard from './PostCard'
 
-export {Header, Footer, Container, Logo, LogoutBtn, RTE};
+export {Header, Footer, Container, Logo, LogoutBtn, RTE, PostForm, Login, Signup, PostCard, PostForm};
